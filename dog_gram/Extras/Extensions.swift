0806bef1f6bdd6e4a00590a5d030e9da0bfc5566
Rick Brown <ricky.brown.00@gmail.com>
2021-09-15 13:27:28 +0100

    adds tab view, and some extensions to Color to facilitate the custom theme

---
+++ dog_gram/Extras/Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  Extensions.swift
+//  dog_gram
+//
+//  Created by Rick Brown on 15/09/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+  struct app {
+    static var purple: Color {
+      Color("ColorPurple")
+    }
+    
+    static var yellow: Color {
+      Color("ColorYellow")
+    }
+    
+    static var beige: Color {
+      Color("BeigeColor")
+    }
+  }
+}
---
+++ dog_gram/Views/Screens/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  dog_gram
+//
+//  Created by Rick Brown on 15/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    TabView {
+      Text("Screen #1")
+        .tabItem {
+          Image(systemName: "book.fill")
+          Text("Feed")
+        }
+      
+      Text("Screen #2")
+        .tabItem {
+          Image(systemName: "magnifyingglass")
+          Text("Browse")
+        }
+      
+      Text("Screen #3")
+        .tabItem {
+          Image(systemName: "square.and.arrow.up.fill")
+          Text("Upload")
+        }
+      
+      Text("Screen #4")
+        .tabItem {
+          Image(systemName: "person.fill")
+          Text("Profile")
+        }
+    }
+    .accentColor(Color.app.purple)
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
