2f597540f21deac7b06cc0c1d46206561891bc38
Rick Brown <ricky.brown.00@gmail.com>
2021-09-15 12:45:41 +0100

    Initial Commit

---
+++ dog_gram/dog_gramApp.swift
@@ -0,0 +1,17 @@
+//
+//  dog_gramApp.swift
+//  dog_gram
+//
+//  Created by Rick Brown on 15/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct dog_gramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
