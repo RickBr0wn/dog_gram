adc39f200a0b2b724d92414ca8f0315adfd13cde
Rick Brown <ricky.brown.00@gmail.com>
2021-09-20 13:40:43 +0100

    adds pods

--- dog_gram/Views/Screens/dog_gramApp.swift
+++ dog_gram/Views/Screens/dog_gramApp.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct dog_gramApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  init() {
+    FirebaseApp.configure()
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
